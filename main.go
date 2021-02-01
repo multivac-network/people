ccc418df806d5de83fdac3911a6cf9616ba3b163
joe.kelly <joe.kelly@outlook.com>
2021-02-01 00:38:40 -0500

    test

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "regexp"
+import "context"
+import "github.com/vartanbeno/go-reddit/reddit"
+
+func main() {
+  ctx := context.Background()
+  posts, _, err := reddit.DefaultClient().Subreddit.RisingPosts(ctx, "wallstreetbets", &reddit.ListOptions{
+			Limit: 100,
+    })
+	if err != nil {
+		return
+	}
+  for _, post := range posts {
+    regex := regexp.MustCompile(`\s([A-Z]{2,4})\s`)
+    matched := regex.MatchString(post.Body)
+    if matched && post.Score >= 100 {
+      vals := regex.FindAllString(post.Body, -1)
+      for _, val := range vals {
+        fmt.Printf("Match: %s\tScore: %d\t%s\n", val, post.Score, post.ID)
+      }
+    }
+  }
+	
+}
