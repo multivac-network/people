5d957c754d3de52a1746938029cb1e72a12cbdfb
joe.kelly <joe.kelly@outlook.com>
2021-02-06 15:33:13 -0500

    removed test method

--- main.go
+++ main.go
@@ -7,14 +7,4 @@ import "repath.io/data"
 func main() {
   configuration := config.LoadConfiguration()
   fmt.Printf("initializing %s\n", configuration.ServiceName)
-  output, _ := helloWorld(configuration.Neo4j.URI, configuration.Neo4j.Username, configuration.Neo4j.Password)
-  print(output)
-}
-
-func helloWorld(uri, username, password string) (interface{}, error) {
-  datastore := data.NewPersonDataStore(uri, username, password)
-  result, _ := datastore.Save(data.Person{FirstName: "Joe", LastName:"Kelly"})
-  datastore.Close()
-
-	return result, nil
 }
