74f7ee8951c400eb425a3f6bf8bffc9152993511
Joe Kelly <joe_kelly@stchome.com>
2021-02-18 18:22:58 -0700

    added http server

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module repath.io
 go 1.12
 
 require (
+	github.com/99designs/gqlgen v0.13.0 // indirect
 	github.com/google/uuid v1.2.0
 	github.com/neo4j/neo4j-go-driver v1.8.3
 )
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+
 	"fmt"
+	"log"
+	"net/http"
 	"repath.io/config"
 	"repath.io/data"
 )
@@ -10,8 +13,16 @@ func main() {
 	configuration := config.LoadConfiguration()
 	fmt.Printf("starting %s service\n", configuration.ServiceName)
 	datastore := data.NewPersonDataStore(configuration.Neo4j.URI, configuration.Neo4j.Username, configuration.Neo4j.Password)
+	defer datastore.Close()
 
 	// initialize API and pass datastore
+	http.HandleFunc("/echo", echoHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
-	defer datastore.Close()
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("hello world"))
+	if err != nil {
+		log.Println(err)
+	}
 }
