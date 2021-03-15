dce15f8cbe06aac0cb8aa0648743d9339e46bc8d
Joe Kelly <joe_kelly@stchome.com>
2021-03-14 20:48:44 -0700

    added authorization layer

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.12
 
 require (
 	github.com/99designs/gqlgen v0.13.0 // indirect
+	github.com/go-chi/chi v3.3.2+incompatible // indirect
 	github.com/google/uuid v1.2.0
 	github.com/neo4j/neo4j-go-driver v1.8.3
 	github.com/neo4j/neo4j-go-driver/v4 v4.2.4 // indirect
+	github.com/repath-io/auth v0.0.0-20210315003403-0cb5900e9c61 // indirect
 	github.com/repath-io/data v0.0.0-20210313232837-2b71174cd82c // indirect
 	github.com/vektah/gqlparser/v2 v2.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/repath-io/auth"
 	"log"
 	"net/http"
 	"os"
@@ -28,11 +30,12 @@ func main() {
 		port = defaultPort
 	}
 
+	router := chi.NewRouter()
+	router.Use(auth.AuthorizationProvider())
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
